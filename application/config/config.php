8a9f144e0bc478633cd366d0bd0770bfa86a544c
Muhammad Bagoes Prastya <bagoesprastya34@gmail.com>
2021-04-04 15:26:45 +0700

    mengubah file config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/';
+$config['base_url'] = 'http://localhost/ciperpus/';
 
 /*
 |--------------------------------------------------------------------------
